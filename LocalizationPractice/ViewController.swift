cd604503e7c5f0a9cd313034fac190b72441427e
Kevin Hou <kevin.ch.hou@gmail.com>
2016-06-14 11:27:20 -0700

    Initial Commit

--- LocalizationPractice/ViewController.swift
+++ LocalizationPractice/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainBodyText: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var testButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mainBodyText.numberOfLines = 0 // Set number of lines to 0 for word wrap
+        testButton.setTitle(NSLocalizedString("Button", comment: "Label on the button"), forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressedTestButton(sender: AnyObject) {
+        mainBodyText.text = NSLocalizedString("This is a hard coded string in English", comment: "This is a test for a hard coded string")
+        testButton.setTitle(NSLocalizedString("Button Was Pressed", comment: "Label on the button"), forState: .Normal)
+    }
 
 }
 
